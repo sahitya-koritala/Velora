0f2120c1fcdb10e99431f9a90ac0aa6dc6cb1e1b
Sahitya <sahityakoritala@gmail.com>
2026-05-18 23:31:50 +0530

    serve frontend

--- backend/server.js
+++ backend/server.js
@@ -38,7 +38,7 @@ const path = require("path");
 
 app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-app.get("*", (req, res) => {
+app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
 });
 
